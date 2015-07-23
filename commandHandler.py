f454eb78b6e5f2dff9e8c0c998081f5fb4ace0e3
Bronco Chen <i@broncotc.com>
2015-07-23 11:13:39 +0800

    make understandble code

--- commandHandler.py
+++ commandHandler.py
@@ -1,27 +1,23 @@
 # encoding: UTF-8
 __author__ = 'bakabie'
 import requests,json
-class worktileAPI():
-	pass
 
-class worktileUserAPI(worktileAPI):
+class worktileUserAPI():
 	def GetUserInfo(self,token):
 		t = requests.get("https://api.worktile.com/v1/user/profile",headers = {"access_token":token})
 		if 'error_code' in t.text:
 			return "error"
 		else:
-				return t.text
-
-class worktileProjectAPI(worktileAPI):
-	def getUserAllProject(self,token):
-
-		t = requests.get("https://api.worktile.com/v1/projects",headers = {"access_token":token})
-		if 'error_code' in t.text:
-			return "error"
-		else:
 			return t.text
-	def getDetail(self,pid,token):
 
+class worktileProjectAPI():
+	def getUserAllProject(self,token):
+			t = requests.get("https://api.worktile.com/v1/projects",headers = {"access_token":token})
+			if 'error_code' in t.text:
+				return "error"
+			else:
+				return t.text
+	def getDetail(self,pid,token):
 		t = requests.get("https://api.worktile.com/v1/projects/"+pid,headers = {"access_token":token})
 		if "error_code" in t.text:
 			return "error"
@@ -51,9 +47,8 @@ class worktileProjectAPI(worktileAPI):
 		else:
 			return t.text
 
-
-class worktileEntryAPI
-	def GetTask(self,pid,token):
+class worktileEntryAPI():
+	def getTask(self,pid,token):
 		data = {
 			"pid":pid,
 			"access_token":token
@@ -63,7 +58,7 @@ class worktileEntryAPI
 			return "error"
 		else:
 			return t.text
-	def CreateTask(self,pid,token,name):
+	def createTask(self,pid,token,name):
 		data = {
 			"name":name
 		}
@@ -72,7 +67,7 @@ class worktileEntryAPI
 			return "error"
 		else:
 			return t.text
-	def Rename(self,entry_id,pid,token,name):
+	def rename(self,entry_id,pid,token,name):
 		data = {
 			"name":name
 		}
@@ -81,61 +76,3 @@ class worktileEntryAPI
 			return "error"
 		else:
 			return t.text
-	def WatchTask(self,entry_id,pid,token):
-		t = requests.post("https://api.worktile.com/v1/entries/"+entry_id+"/watcher?pid="+pid+"&access_token="+token)
-		if "error_code" in t.text:
-			return "error"
-		else:
-			return t.text
-	def DelTask(self,entry_id,pid,token):
-		t = requests.delete("https://api.worktile.com/v1/entries/"+entry_id+"?pid="+pid+"&access_token="+token)
-		if "error_code" in t.text:
-			return "error"
-		else:
-			return t.text
-	def ReWatchTask(self,entry_id,pid,token):
-		t =requests.delete("https://api.worktile.com/v1/entries/"+entry_id+"/watcher?pid="+pid+"&access_token="+token)
-		if "error_code" in t.text:
-			return "error"
-		else:
-			return t.text
-class worktileTaskAPI():
-	def GetTaskList(self,token,pid,type):
-		data = {
-			"access_token":token,
-			"pid":pid,
-			"type":type
-		}
-		t = requests.get("https://api.worktile.com/v1/tasks",data = data)
-		if "error_code" in t.text:
-			return "error"
-		else:
-			return t.text
-	def WillOverTask(self,token):
-		t= requests.get("https://api.worktile.com/v1/tasks/today?access_token="+token)
-		if "error_code" in t.text
-			return "error"
-		else:
-			return t.text
-	def CreatNewTask(self,pid,token,name,entry_id):
-		data = {
-			"name":name,
-			"entry_id":entry_id
-		}
-		t=requests.post("https://api.worktile.com/v1/task?pid="+pid+"&access_token="+token)
-		if "error_code" in t.text
-			return "error"
-		else:
-			return t.text
-	def TaskDet(self,pid,token,tid):
-		t=requests.get("https://api.worktile.com/v1/tasks/"+tid+"?access_token="+token+"&pid="+pid)
-		if "error_code" in t.text
-			return "error"
-		else:
-			return t.text
-	def ReTask(self,tid,pid,token,name,desc):
-		data = {
-			"name":name,
-			"desc":desc
-		}
-		t = requests.put("https://api.worktile.com/v1/tasks/"+tid+"?pid="+pid+"&access_token="+token)
