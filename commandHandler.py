ee3c0dca9753b50d2c87f499f9675b077103851a
bakabie <blackguwc@163.com>
2015-07-30 20:29:15 +0800

    is this?

--- commandHandler.py
+++ commandHandler.py
@@ -1,42 +1,46 @@
 # encoding: UTF-8
 __author__ = 'bakabie'
 import requests
+import json
 
 
 class worktileUserAPI():
 	def GetUserInfo(self, token):
-		t = requests.get("https://api.worktile.com/v1/user/profile", headers={"access_token": token})
+		t = requests.get("https://api.worktile.com/v1/user/profile", params={"access_token": token})
 		if 'error_code' in t.text:
 			raise ValueError
 		else:
-			return t.text
-
+			return json.dumps(t.text)
 
 class worktileProjectAPI():
 	def getUserAllProject(self, token):
+<<<<<<< HEAD
 		t = requests.get("https://api.worktile.com/v1/projects", headers={"access_token": token})
 		print t
 		print t.text
+=======
+		t = requests.get("https://api.worktile.com/v1/projects", parmas={"access_token": token})
+>>>>>>> is this?
 		if 'error_code' in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dump(t.text)
 
-	def getDetail(self, pid, token):
+	def getProjectDetail(self, pid, token):
 		t = requests.get("https://api.worktile.com/v1/projects/" + pid, headers={"access_token": token})
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
-	def getMember(self, pid, token):
+	def getProjectMember(self, pid, token):
 		t = requests.get("https://api.worktile.com/v1/projects/" + pid + "/members", headers={"access_token": token})
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
-	def addMember(self, pid, uid, role, token):
+	def addProjectMember(self, pid, uid, role, token):
 		data = {
 			"uid": uid,
 			"role": role,
@@ -46,19 +50,19 @@ class worktileProjectAPI():
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
-	def delMember(self, pid, uid, token):
+	def delpRrojectMember(self, pid, uid, token):
 		t = requests.delete("https://api.worktile.com/v1/projects/" + pid + "/members/" + uid,
 							headers={"access_token": token})
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
 
 class worktileEntryAPI():
-	def getTask(self, pid, token):
+	def getEntryTask(self, pid, token):
 		data = {
 			"pid": pid,
 			"access_token": token
@@ -67,55 +71,55 @@ class worktileEntryAPI():
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
-	def createTask(self, pid, token, name):
+	def createEntryTask(self, pid, token, name):
 		data = {
 			"name": name
 		}
-		t = requests.post("https://api.worktile.com/v1/entries?pid=" + pid + "&access_token=" + token)
+		t = requests.post("https://api.worktile.com/v1/entries",params={"pid" : pid ,"access_token" : token},data=data)
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
-	def rename(self, entry_id, pid, token, name):
+	def reEntryname(self, entry_id, pid, token, name):
 		data = {
 			"name": name
 		}
-		t = requests.put("https://api.worktile.com/v1/entries/" + entry_id + "?pid=" + pid + "&access_token=" + token)
+		t = requests.put("https://api.worktile.com/v1/entries/" + entry_id ,params={ "pid" : pid , "access_token" : token},data=data)
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
-	def watchTask(self, entry_id, pid, token):
+	def watchEntryTask(self, entry_id, pid, token):
 		t = requests.post(
-			"https://api.worktile.com/v1/entries/" + entry_id + "/watcher?pid=" + pid + "&access_token=" + token)
+			"https://api.worktile.com/v1/entries/" + entry_id + "/watcher",params={"pid" : pid , "access_token" : token})
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
-	def delTask(self, entry_id, pid, token):
+	def delEntryTask(self, entry_id, pid, token):
 		t = requests.delete(
-			"https://api.worktile.com/v1/entries/" + entry_id + "?pid=" + pid + "&access_token=" + token)
+			"https://api.worktile.com/v1/entries/" + entry_id , params={"pid" : pid , "access_token" : token})
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
-	def reWatchTask(self, entry_id, pid, token):
+	def reWatchEntryTask(self, entry_id, pid, token):
 		t = requests.delete(
-			"https://api.worktile.com/v1/entries/" + entry_id + "/watcher?pid=" + pid + "&access_token=" + token)
+			"https://api.worktile.com/v1/entries/" + entry_id + "/watcher",params={"pid" : pid , "access_token" : token})
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
 
 class worktileTaskAPI():
-	def getList(self, token, pid, type):
+	def getTaskList(self, token, pid, type):
 		data = {
 			"access_token": token,
 			"pid": pid,
@@ -125,47 +129,47 @@ class worktileTaskAPI():
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
 	def willOverTask(self, token):
-		t = requests.get("https://api.worktile.com/v1/tasks/today?access_token=" + token)
+		t = requests.get("https://api.worktile.com/v1/tasks/today",params={"access_token" : token})
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
 	def createTask(self, pid, token, name, entry_id):
 		data = {
 			"name": name,
 			"entry_id": entry_id
 		}
-		t = requests.post("https://api.worktile.com/v1/task?pid=" + pid + "&access_token=" + token)
+		t = requests.post("https://api.worktile.com/v1/task",params={"pid" : pid , "access_token" : token},data=data)
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
 	def taskDetail(self, pid, token, tid):
-		t = requests.get("https://api.worktile.com/v1/tasks/" + tid + "?access_token=" + token + "&pid=" + pid)
+		t = requests.get("https://api.worktile.com/v1/tasks/" + tid , params={"access_token" : token , "pid" : pid})
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
 	def editTask(self, tid, pid, token, name, desc):
 		data = {
 			"name": name,
 			"desc": desc
 		}
-		t = requests.put("https://api.worktile.com/v1/tasks/" + tid + "?pid=" + pid + "&access_token=" + token)
+		t = requests.put("https://api.worktile.com/v1/tasks/" + tid , params = {"pid" : pid ,"access_token" : token},data=data)
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
 
 	def deleteTask(self, tid, pid, token):
-		t = requests.delete("https://api.worktile.com/v1/tasks/" + tid + "?pid=" + pid + "&access_token=" + token)
+		t = requests.delete("https://api.worktile.com/v1/tasks/" + tid ,params={"pid" : pid , "access_token" : token})
 		if "error_code" in t.text:
 			raise ValueError
 		else:
-			return t.text
+			return json.dumps(t.text)
